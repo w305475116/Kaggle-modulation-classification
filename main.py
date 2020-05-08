4dc991104b409744f4de9956149adde7cd15ca2b
w305475116 <w305475116@gmail.com>
2020-05-07 21:41:21 -0400

    change the model evaluation and plot

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import csv
 import time
 import h5py
+from keras.callbacks import Callback, ModelCheckpoint
 from keras.models import Sequential
 from keras.layers import Dense, Dropout
 from keras.layers import Embedding
@@ -9,14 +10,11 @@ import numpy as np
 import pandas as pd
 import CONSTANTS
 from keras.utils import to_categorical
-from keras.models import load_model
-
-
+import matplotlib.pyplot as plt
 
 f = h5py.File("./dataset/data.hdf5", 'r')
 
 x_train = np.array(f['train'])
-
 x_test =  np.array(f['test'])
 
 y_train = []
@@ -28,7 +26,6 @@ for line in train_labels.values:
 x_train = np.append(x_train, np.flip(x_train), axis = 0)
 y_train = np.append(y_train, np.flip(y_train))
 y_train = to_categorical(np.array(y_train), num_classes=10)
-
 
 
 model = Sequential()
@@ -44,17 +41,20 @@ model.add(Dropout(0.5))
 # model.add(Dense(2048, activation='relu'))
 model.add(Dense(10, activation='softmax'))
 
+# keras.optimizers.RMSprop(learning_rate=0.001, rho=0.9)
 model.compile(loss='categorical_crossentropy',
               optimizer='rmsprop',
               metrics=['accuracy'])
 
 # model = load_model("models/Tue_May__5_14:50:42_2020")
-
-model.fit(x_train, y_train, batch_size=16, epochs=15)
+startT = time.asctime().replace(" ","_")
+checkpointer = ModelCheckpoint(filepath='./models/startAtBest' + startT + '.hdf5', verbose=1, save_best_only=True)
+history = model.fit(x_train, y_train, validation_split=0.25, batch_size=16, epochs=1, callbacks=[checkpointer])
 finishT = time.asctime().replace(" ","_")
 model.save("./models/"+finishT)
 y_test = model.predict_classes(x_test)
 # print(y_test)
+
 with open('./predicts/'+finishT+'.csv', 'w') as csvfile:
     filewriter = csv.writer(csvfile, delimiter=',',
                             quotechar='|', quoting=csv.QUOTE_MINIMAL)
@@ -62,3 +62,22 @@ with open('./predicts/'+finishT+'.csv', 'w') as csvfile:
     for i in range(len(y_test)):
         filewriter.writerow([i, CONSTANTS.DICT_ITER[y_test[i]]])
 
+# 绘制训练 & 验证的准确率值
+plt.plot(history.history['accuracy'])
+plt.plot(history.history['val_accuracy'])
+plt.title('Model accuracy')
+plt.ylabel('Accuracy')
+plt.xlabel('Epoch')
+plt.legend(['Train', 'Test'], loc='upper left')
+plt.show()
+plt.savefig('./plots/'+finishT+'acc.png')
+
+# 绘制训练 & 验证的损失值
+plt.plot(history.history['loss'])
+plt.plot(history.history['val_loss'])
+plt.title('Model loss')
+plt.ylabel('Loss')
+plt.xlabel('Epoch')
+plt.legend(['Train', 'Test'], loc='upper left')
+plt.show()
+plt.savefig('./plots/'+finishT+'err.png')
